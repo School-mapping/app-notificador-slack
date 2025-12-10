77127393525d1cdf0534f6b8caac35cb482ccdbf
KauanLuna <kauan.luna@sptech.school>
2025-12-10 14:58:09 -0300

    Criando verificação caso ultimo disparo seja nulo

--- notificador-slack/src/main/java/entity/Monitor.java
+++ notificador-slack/src/main/java/entity/Monitor.java
@@ -8,20 +8,21 @@ import java.time.LocalDateTime;
 public class Monitor {
     private BancoRepositorio banco = new BancoRepositorio();
 
-    public Boolean verificarCondicao(Configuracao config){
-
-        if ("NOVAS_VERBAS".equalsIgnoreCase(config.getTipoAlerta())){
-            return banco.existemNovasVerbas(config.getUltimoDisparo() == null ? LocalDateTime.now() : config.getUltimoDisparo());
+    public Boolean verificarCondicao(Configuracao config) {
+        if (config.getUltimoDisparo() == null) {
+            return true;
+        } else if ("NOVAS_VERBAS".equalsIgnoreCase(config.getTipoAlerta())) {
+            return banco.existemNovasVerbas(config.getUltimoDisparo());
         } else if ("NOVAS_ESCOLAS".equalsIgnoreCase(config.getTipoAlerta())) {
-            return banco.existemNovasEscolas(config.getUltimoDisparo() == null ? LocalDateTime.now() : config.getUltimoDisparo());
+            return banco.existemNovasEscolas(config.getUltimoDisparo());
         } else if ("NOVAS_NOTAS".equalsIgnoreCase(config.getTipoAlerta())) {
-            return banco.existemNovasNotas(config.getUltimoDisparo() == null ? LocalDateTime.now() : config.getUltimoDisparo());
+            return banco.existemNovasNotas(config.getUltimoDisparo());
         }
 
         return false;
     }
 
-    public String gerarMensagem(Configuracao config){
+    public String gerarMensagem(Configuracao config) {
         if ("NOVAS_VERBAS".equalsIgnoreCase(config.getTipoAlerta())) {
             return "🚨 Alerta School Mapping: Novas verbas detectadas!";
         } else if ("NOVAS_ESCOLAS".equalsIgnoreCase(config.getTipoAlerta())) {
